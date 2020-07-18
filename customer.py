241a972b61d5c508817f128842889f2e3216b187
Saeid Gholami <saeidxgholami@gmail.com>
2020-07-18 11:03:50 +0430

    customer

---
+++ customer.py
@@ -0,0 +1,11 @@
+class Customer:
+	def __init__(self, first_name, last_name, cell_phone, address):
+		self.first_name = first_name
+		self.last_name = last_name
+		self.cell_phone = cell_phone
+		self.address = address
+
+	def create(self):
+		""" Create a customer in the db """
+		
+
